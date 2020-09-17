4ab4bdd33f01fef51c7115a65b8feecf2395fad0
Nguyen Hung <billy7d95@gmail.com>
2020-09-17 15:46:50 +0700

    add

---
+++ Week 2/Serverlet_tudien/src/main/java/ServletTuDien.java
@@ -0,0 +1,41 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "ServletTuDien",urlPatterns = "/translate")
+public class ServletTuDien extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello","Xin chao");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển vở");
+        dictionary.put("computer", "Máy tính");
+
+       String search = request.getParameter("txtSearch");
+
+       PrintWriter printWriter = response.getWriter();
+       printWriter.println("<html>");
+
+       String result = dictionary.get(search);
+
+       if (result!=null){
+           printWriter.println("Word: "+search);
+           printWriter.println("Result: "+result);
+       } else {
+           printWriter.println("Not found");
+       }
+
+       printWriter.println("</html>");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
